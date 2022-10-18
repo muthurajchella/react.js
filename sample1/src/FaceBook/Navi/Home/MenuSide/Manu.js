288cb0550cddb634d01c35f6de372f8325a3f419
Muthurajchella- <muthuraj.chella@gmail.com>
2022-10-19 02:02:48 +0530

    FaceBook

--- sample1/src/FaceBook/Navi/Home/MenuSide/Manu.js
+++ sample1/src/FaceBook/Navi/Home/MenuSide/Manu.js
@@ -1,74 +1,84 @@
 import React from 'react'
 import './Manu.css'
 
+
 const Manu = () => {
   return (
     <div className='manu'>
+    <div className='manuScroll'>
       <div className='manu-flex'>
         <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
         <div><h1>Profile</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='https://cdn.iconscout.com/icon/premium/png-256-thumb/friend-3197606-2670100.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/S0U5ECzYUSu.png'></img></div>
         <div><h1>Friends</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='https://us.123rf.com/450wm/jovanas/jovanas1607/jovanas160700554/60195725-bandiera-icona.jpg?ver=6'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yH/r/kyCAf2jbZvF.png'></img></div>
         <div><h1>Pages</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='https://www.pngitem.com/pimgs/m/105-1052269_facebook-groups-facebook-groups-logo-png-transparent-png.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/PrjLkDYpYbH.png'></img></div>
         <div><h1>Groups</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='https://www.howtogeek.com/wp-content/uploads/2022/07/Facebook-Marketplace.png?height=200p&trim=2,2,2,2&crop=16:9'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yU/r/D2y-jJ2C_hO.png'></img></div>
         <div><h1>Marketplace</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='https://cdn.iconscout.com/icon/premium/png-256-thumb/history-2843409-2363708.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/he-BkogidIc.png'></img></div>
         <div><h1>Memories</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/lVijPkTeN-r.png'></img></div>
+        <div><h1>Saved</h1></div>
+      </div>
+      <div className='manu-flex'>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/uhteIEYDeQx.png'></img></div>
         <div><h1>Reels</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yK/r/mAnT0r8GSOm.png'></img></div>
+        <div><h1>Favorites</h1></div>
+      </div>
+      <div className='manu-flex'>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/4Y9Xi2D3hJv.png'></img></div>
         <div><h1>Messenger</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yu/r/1Xvrz50fHMF.png'></img></div>
         <div><h1>Messenger Kids</h1></div>
       </div> 
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/CpdWrf1nzR2.png'></img></div>
         <div><h1>Blood Donations</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/y3/r/Jr0q8qKF2-Y.png'></img></div>
         <div><h1>Climate Science Center</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/5rR6LRpNc5u.png'></img></div>
         <div><h1>COVID-19 Help Center</h1></div>
       </div> 
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/y4/r/p1cXh6javjn.png'></img></div>
         <div><h1>Crisis Response</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/vxMUnHhu6Do.png'></img></div>
         <div><h1>Emotional Help</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yb/r/eECk3ceTaHJ.png'></img></div>
         <div><h1>Most Recent</h1></div>
       </div>
       <div className='manu-flex'>
-        <div><img src='http://keyword-hero.com/wp-content/uploads/2017/04/Cart-Hero.png'></img></div>
+        <div><img src='https://static.xx.fbcdn.net/rsrc.php/v3/yt/r/PObY9OA5lvJ.png'></img></div>
         <div><h1>Play Game</h1></div>
       </div>
-      
+      </div>
     </div>
   )
 }
